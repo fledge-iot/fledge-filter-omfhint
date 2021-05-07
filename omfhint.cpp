c83a995eae8a6f50e6436f0cfa5d9f0791edba3d
Stefano <stefano@dianomic.com>
2021-05-07 13:41:53 +0000

    FOGL-5383: code cleanup
    Signed-off-by: Stefano <stefano@dianomic.com>

--- omfhint.cpp
+++ omfhint.cpp
@@ -58,19 +58,8 @@ OMFHintFilter::ingest(vector<Reading *> *readings, vector<Reading *>& out)
 		string name = (*elem)->getAssetName();
 		auto it = m_hints.find(name);
 
-		//# FIXME_I
-		Logger::getLogger()->setMinLevel("debug");
-		Logger::getLogger()->debug("xx3 %s - v2 :%s:", __FUNCTION__, name.c_str() );
-		Logger::getLogger()->setMinLevel("warning");
-
-
 		if (it != m_hints.end())
 		{
-			//# FIXME_I
-			Logger::getLogger()->setMinLevel("debug");
-			Logger::getLogger()->debug("xxx3 %s - v2 :%s: HINT ", __FUNCTION__, name.c_str() );
-			Logger::getLogger()->setMinLevel("warning");
-
 			HintMatch = true;
 			DatapointValue value(it->second);
 			(*elem)->addDatapoint(new Datapoint("OMFHint", value));
@@ -79,40 +68,18 @@ OMFHintFilter::ingest(vector<Reading *> *readings, vector<Reading *>& out)
 
 			if ( ! m_wildcards.empty() ) {
 
-				//# FIXME_I
-				Logger::getLogger()->setMinLevel("debug");
-				Logger::getLogger()->debug("xxx3 %s - v2 :%s: Wildcars hint  ", __FUNCTION__, name.c_str() );
-				Logger::getLogger()->setMinLevel("warning");
-
 				for(auto &item : m_wildcards) {
 
-					//# FIXME_I
-					Logger::getLogger()->setMinLevel("debug");
-					Logger::getLogger()->debug("xxx3 %s - v2 :%s: Wildcars hint  element ", __FUNCTION__, name.c_str() );
-					Logger::getLogger()->setMinLevel("warning");
-
-					if (std::regex_match (name, item.first)) {
-
-						//# FIXME_I
-						Logger::getLogger()->setMinLevel("debug");
-						Logger::getLogger()->debug("xxx3 %s - v2 :%s: Wildcars hint  MATCh ", __FUNCTION__, name.c_str() );
-						Logger::getLogger()->setMinLevel("warning");
-
-						HintMatch = true;
+					if (std::regex_match (name, item.first))
+					{
 						DatapointValue value(item.second);
 						(*elem)->addDatapoint(new Datapoint("OMFHint", value));
 						AssetTracker::getAssetTracker()->addAssetTrackingTuple(m_name, name, string("Filter"));
+						break;
 					}
 				}
 			}
 		}
-
-		// FIXME_I:
-//		if (HintMatch) {
-//
-//			(*elem)->addDatapoint(new Datapoint("OMFHint", value));
-//			AssetTracker::getAssetTracker()->addAssetTrackingTuple(m_name, name, string("Filter"));
-//		}
 
 		out.push_back(*elem);
 	}
@@ -133,29 +100,14 @@ OMFHintFilter::reconfigure(const string& newConfig)
 	configure(config);
 }
 
-/**
- * Evaluates if the input string is a regular expression
- */
-//bool IsRegex(const string &str) {
-//
-//	size_t nChar;
-//	nChar = strcspn(str.c_str(), "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_");
-//
-//	Logger::getLogger()->setMinLevel("debug");
-//	Logger::getLogger()->debug("xxx4 %s - nChar :%d:",  __FUNCTION__, nChar);
-//	Logger::getLogger()->setMinLevel("warning");
-//
-//	return (nChar != 0);
-//
-//}
-
-
 void
 OMFHintFilter::configure(const ConfigCategory& config)
 {
 	if (config.itemExists("hints"))
 	{
 		m_hints.clear();
+		m_wildcards.clear();
+
 		Document doc;
 		ParseResult result = doc.Parse(config.getValue("hints").c_str());
 		if (!result)
@@ -182,26 +134,11 @@ OMFHintFilter::configure(const ConfigCategory& config)
 					pos = escaped.find("\"", pos+replace.size());
 				}
 
-				//# FIXME_I
-				Logger::getLogger()->setMinLevel("debug");
-				Logger::getLogger()->debug("xxx2 %s - v2 :%s: :%s: ", __FUNCTION__, asset.c_str() ,escaped.c_str() );
-				Logger::getLogger()->setMinLevel("warning");
-
-				// FIXME_I:
 				if (IsRegex(asset)) {
-
-					//# FIXME_I
-					Logger::getLogger()->setMinLevel("debug");
-					Logger::getLogger()->debug("xxx2 %s - REG",  __FUNCTION__);
-					Logger::getLogger()->setMinLevel("warning");
 
 					m_wildcards.push_back(std::pair<std::regex, std::string>(std::regex(asset), escaped));
 				} else {
 					m_hints.insert(pair<string, string>(asset, escaped));
-					//# FIXME_I
-					Logger::getLogger()->setMinLevel("debug");
-					Logger::getLogger()->debug("xxx2 %s - NO reg ", __FUNCTION__);
-					Logger::getLogger()->setMinLevel("warning");
 				}
 			}
 		}
