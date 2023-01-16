bf90b9663578be781ad2bf87bcca44f24fceb55a
nandan <nandan.ghildiyal@gmail.com>
2023-01-16 13:53:28 +0530

    FOGL-7272 : Filter unit tests written
    Signed-off-by: nandan <nandan.ghildiyal@gmail.com>

---
+++ tests/README.rst
@@ -0,0 +1,13 @@
+=====================================================
+Build and Run test cases
+=====================================================
+
+To build Fledge "omfhint" C++ filter plugin unit test cases:
+
+.. code-block:: console
+
+  $ mkdir build
+  $ cd build
+  $ cmake ..
+  $ make
+  $ ./RunTests
---
+++ tests/test_info.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include <plugin_api.h>
+#include <string.h>
+#include <string>
+#include <rapidjson/document.h>
+
+using namespace std;
+using namespace rapidjson;
+
+extern "C"
+{
+	PLUGIN_INFORMATION *plugin_info();
+};
+
+TEST(OMFHINT_INFO, PluginInfo)
+{
+	PLUGIN_INFORMATION *info = plugin_info();
+	ASSERT_STREQ(info->name, "omfhint");
+	ASSERT_EQ(info->type, PLUGIN_TYPE_FILTER);
+}
+
+TEST(OMFHINT_INFO, PluginInfoConfigParse)
+{
+	PLUGIN_INFORMATION *info = plugin_info();
+	Document doc;
+	doc.Parse(info->config);
+	ASSERT_EQ(doc.HasParseError(), false);
+	ASSERT_EQ(doc.IsObject(), true);
+	ASSERT_EQ(doc.HasMember("plugin"), true);
+}
